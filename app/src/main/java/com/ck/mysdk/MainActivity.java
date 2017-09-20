bc6540a6830b2ebc30ae3f261a052fe5eeba0d11
ck <1234556>
2017-09-20 20:45:45 +0800

    ddd

--- app/src/main/java/com/ck/mysdk/MainActivity.java
+++ app/src/main/java/com/ck/mysdk/MainActivity.java
@@ -1,22 +1,20 @@
 package com.ck.mysdk;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.ck.mylibrary.base.BaseActivity;
-import com.ck.mylibrary.utils.ToastUtils;
 
-public class MainActivity extends BaseActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        needImmersive=false;
-        setImmersiveStatus(R.color.color_00a5eb);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main,true);
+        setContentView(R.layout.activity_main);
     }
 
     public void sdk(View view) {
-        ToastUtils.showToast(this,"哈哈哈");
+
     }
 }
